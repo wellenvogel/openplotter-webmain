be6dac724c1562a3f53b5d01ceeecee58e81aa15
andreas <andreas@wellenvogel.de>
2021-01-28 17:23:58 +0100

    iptables for localhost, cursor

--- ipTables80.sh
+++ ipTables80.sh
@@ -15,17 +15,27 @@ err(){
 }
 
 #check for existance
-old=`iptables -L PREROUTING -n -t nat --line-numbers | grep "dpt:80  *redir  *ports  *$port" | sed 's/ .*//'`
-if [ "$old" = "" ] ; then
-  if [ $enable = enable ] ; then
-    echo "enabling forward from port 80 to $port"
+old=`iptables -L PREROUTING -n -t nat --line-numbers | grep "dpt:80  *redir  *ports  *$port" | head -1 | sed 's/ .*//'`
+oldi=`iptables -L OUTPUT -v -t nat --line-numbers -n | grep "dpt:80  *redir  *ports  *$port" | head -1 | sed 's/ .*//'`
+if [ $enable = enable ] ; then
+  if [ "$old" = "" ] ; then
+    echo "enabling forward from port 80 to $port ext"
     iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports "$port" || err "iptables insert failed"
     echo "forwarding to port $port added"
   fi
+  if [ "$oldi" = "" ] ; then
+    echo "enabling forward from port 80 to $port int"
+    iptables -t nat -I OUTPUT -p tcp -o lo --dport 80 -j REDIRECT --to-ports "$port" || err "iptables insert failed"
+    echo "forwarding to port $port added"
+  fi  
 else
-  if [ $enable != enable ] ; then
+  if [ "$old" != "" ] ; then
     iptables -t nat -D PREROUTING "$old" || err "deleting forward from iptables failed"
-    echo "forwarding to port $port removed"
+    echo "ext forwarding to port $port removed"
+  fi
+  if [ "$oldi" != "" ] ; then
+    iptables -t nat -D OUTPUT "$oldi" || err "deleting forward from iptables failed"
+    echo "int forwarding to port $port removed"
   fi
 fi
 exit 0
